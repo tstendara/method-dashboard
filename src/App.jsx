5590709da61bd6eb5d13eb6e505ec58f72194712
Tyler Stendara <tstendara@gmail.com>
2023-03-15 00:59:49 -0500

    extract report list to component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import './App.css'
 import { useState, useEffect } from 'react'
 import Button from '@mui/material/Button';
 import { DataGrid } from '@mui/x-data-grid';
-import { Alert, Box, LinearProgress } from '@mui/material';
+import { Box, LinearProgress } from '@mui/material';
 
 import AlertDialog from './AlertDialog.jsx'
 import XmlHandler from './xmlParser.jsx'
 import ResponsiveAppBar from './Header.jsx';
 import ReportList from './ReportList';
-
-// import API from './api/index.js';
-
-// const api = new API();
 
 function Dashboard() {
   const [data, setData] = useState([]);
@@ -21,16 +17,7 @@ function Dashboard() {
   const [reports, setReports] = useState([]);
   const [methodResp, setMethodResp] = useState({});
   const [openAlert, setOpenAlert] = useState(false);
-  
-  // useEffect(() => {
-  //   // get reports from db
-  //   api.getFiles().then((resp) => {
-  //     // console.log(resp)
-  //     setReports(resp)
-  //   })
-  // }, [methodResp]) // put update for when new report is added 
 
-  const resetCount = () => setLoading(0) // passninto xml parser 
   const handlePaymentSummary = async() => setOpenAlert(true)
 
   return (
@@ -89,8 +76,5 @@ function Dashboard() {
   </>
   );
 }
-
-// create a new file xmlHandler.js in src folder that contains a react component that will convert xml file to json file
-
 
 export default Dashboard
