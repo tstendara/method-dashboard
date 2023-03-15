52fe37a086f3235190319884c885f71972e54591
Tyler Stendara <tstendara@gmail.com>
2023-03-15 01:26:52 -0500

    adding error catching/remove optional config values

--- src/api/index.js
+++ src/api/index.js
@@ -3,23 +3,28 @@ import axios from 'axios';
 export default class API {
     constructor() {
         this.axios = axios.create({
-            baseURL: 'http://localhost:3000',
-            timeout: 1000,
-            headers: {
-                'Content-Type': 'application/json',
-            }
+            baseURL: 'http://localhost:3000'
         });
     }
 
     getFiles = async () => {
-        const response = await this.axios.get('/allFiles');
-        return response.data;
+        try{
+            const response = await this.axios.get('/allFiles');
+            return response.data;
+        }
+        catch(err){
+            console.log(err)
+        }
     }
 
     getReportByID = async (id) => {
-        const response = await this.axios.get(`/report/${id}`);
-        console.log(response.data);
-        return response.data;
+        try{
+            const response = await this.axios.get(`/report/${id}`);
+            return response.data;
+        }
+        catch(err){
+            console.log(err)
+        }
     }
 }
 
